31d8ca8a984246aa0629046a16387c3a185bceee
wrotich <winniecherop@outlook.com>
2019-06-27 14:58:54 +0300

    feat(createLinkMutation): users can create links
    - add mutations for creating links
    - add resolvers for the mutations
    
    [Finishes #3]

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -10,7 +10,8 @@ module Mutations
     def resolve(description: nil, url: nil)
       Link.create!(
         description: description,
-        url: url
+        url: url,
+        user: context[:current_user]
       )
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_094539) do
+ActiveRecord::Schema.define(version: 2019_07_16_073039) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "links", "users"
 end

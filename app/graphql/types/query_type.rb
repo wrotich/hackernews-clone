5533d0a8057e494451b4bfb06137b82484fe0198
wrotich <winniecherop@outlook.com>
2019-06-27 14:23:27 +0300

    feat(showingLinks): show a list of links
    - setup links
    - write a query for returning links
    - add a query reolver
    
    [WIP #1]

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_links, [LinkType], null: false
+    # this method is invoked, when 'all_links' field is being resolved
+    def all_links
+      Link.all
     end
   end
 end

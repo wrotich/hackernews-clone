1493ca536ef7720671d208ceabaf2268d4f09775
Winnie Rotich <winniecherop@outlook.com>
2019-07-26 12:14:44 +0300

    feat(errorHandling): add error handling
    - add specific error handling messages
    
    [Finishes #10]

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user, optional: true # Prevent ActiveRecord::RecordInvalid
   has_many :votes
+
+  validates :url, presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 10}
 end

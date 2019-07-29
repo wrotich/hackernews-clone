a5b32dffe8f6189295d55b05c3d366b419e4494f
wrotich <winniecherop@outlook.com>
2019-07-29 10:53:26 +0300

    feat(filtering): add filtering functionality
    - allow users to search through the links
    - search in the description or the url
    
    [Finishes #21]

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,5 @@
 module Types
-  class QueryType < Types::BaseObject
-    field :all_links, [LinkType], null: false
-    # this method is invoked, when 'all_links' field is being resolved
-    def all_links
-      Link.all
-    end
+  class QueryType < BaseObject
+    field :all_links, function: Resolvers::LinksSearch
   end
 end
